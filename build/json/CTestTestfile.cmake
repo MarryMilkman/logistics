57ed3aea886725da85e539da71dabd2d3221a8b5
seletskiy99 <si04021999@gmail.com>
2019-05-28 12:05:31 +0300

    del build and json -_-

--- build/json/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/all_with_prog/logistics/json
-# Build directory: C:/all_with_prog/logistics/build/json
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
